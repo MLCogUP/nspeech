84737c0284f519461d947383f54b8775b298034b
rknaebel <rknaebel@uni-potsdam.de>
2018-02-08 13:33:25 +0100

    add initial version of wavenet to models

--- neural_speech/models/__init__.py
+++ neural_speech/models/__init__.py
@@ -1,11 +1,14 @@
 from models.tacotron import Tacotron
 from models.tacotron2 import Tacotron2
+from models.wavenet import WaveNetModel
 
 
 def create_model(name, hparams):
-    if name == 'tacotron':
+    if name == 'taco1':
         return Tacotron(hparams)
-    if name == 'tacotron2':
+    if name == 'taco2':
         return Tacotron2(hparams)
+    if name == 'wavenet':
+        return WaveNetModel(hparams)
     else:
         raise Exception('Unknown model: ' + name)
