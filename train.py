657d9d7a723492e1975ae17ad5d3c151c03d8aa4
rknaebel <rknaebel@uni-potsdam.de>
2018-03-19 13:48:21 +0100

    replace underscore with minus in command line arguments

--- train.py
+++ train.py
@@ -115,7 +115,7 @@ def train(log_dir, args):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--log_dir', default=os.path.expanduser('logs'))
+    parser.add_argument('--log-dir', default=os.path.expanduser('logs'))
     parser.add_argument('--input', default='../data/train.txt')
     parser.add_argument('--vctk', default='')
     parser.add_argument('--ljspeech', default='', help="Related to preprocessed wav files")
@@ -125,13 +125,13 @@ def main():
     parser.add_argument('--name', help='Name of the run. Used for logging. Defaults to model name.')
     parser.add_argument('--hparams', default='',
                         help='Hyperparameter overrides as a comma-separated list of name=value pairs')
-    parser.add_argument('--restore_step', type=int, help='Global step to restore from checkpoint.')
-    parser.add_argument('--summary_interval', type=int, default=1000,
+    parser.add_argument('--restore-step', type=int, help='Global step to restore from checkpoint.')
+    parser.add_argument('--summary-interval', type=int, default=1000,
                         help='Steps between running summary ops.')
-    parser.add_argument('--checkpoint_interval', type=int, default=1000,
+    parser.add_argument('--checkpoint-interval', type=int, default=1000,
                         help='Steps between writing checkpoints.')
-    parser.add_argument('--slack_url', help='Slack webhook URL to get periodic reports.')
-    parser.add_argument('--tf_log_level', type=int, default=1, help='Tensorflow C++ log level.')
+    parser.add_argument('--slack-url', help='Slack webhook URL to get periodic reports.')
+    parser.add_argument('--tf-log-level', type=int, default=1, help='Tensorflow C++ log level.')
     parser.add_argument('--git', action='store_true', help='If set, verify that the client is clean.')
     parser.add_argument('--gpu', default=0, type=int, help='Select gpu for computation')
     parser.add_argument('--threads', default=1, type=int, help='Select number of threads for enqueue operation')
